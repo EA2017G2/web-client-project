e5e84696788c387aa06e717edc7d8b4c778f9550
xarilo <xavier.rins@gmail.com>
2018-01-10 23:12:20 +0100

    solved problem web calling ionic

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,5 +6,15 @@ import { Http, Response } from '@angular/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  fileInput: any;
+  upload() {
+    let fileBrowser = this.fileInput.nativeElement;
+    if (fileBrowser.files && fileBrowser.files[0]) {
+      const formData = new FormData();
+      formData.append("image", fileBrowser.files[0]);
+     // this.projectService.upload(formData, this.project.id).subscribe(res => {
+        // do stuff w/my uploaded file
+     // });
+    }
+  }
 }
