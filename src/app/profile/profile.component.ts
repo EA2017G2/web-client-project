e55b713fe7984a513e64e5361246a87c0eaa8571
Carlos <carlosmonteroreina@gmail.com>
2018-01-17 19:28:02 +0100

    Merge branch 'master' of C:\Users\Carlos\Desktop\web-client-project with conflicts.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,15 +6,6 @@ import { Http, Response } from '@angular/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  fileInput: any;
-  upload() {
-    let fileBrowser = this.fileInput.nativeElement;
-    if (fileBrowser.files && fileBrowser.files[0]) {
-      const formData = new FormData();
-      formData.append("image", fileBrowser.files[0]);
-     // this.projectService.upload(formData, this.project.id).subscribe(res => {
-        // do stuff w/my uploaded file
-     // });
-    }
-  }
+  sex = ['man', 'woman', 'other'];
+
 }
